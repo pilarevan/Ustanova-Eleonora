eee2b613639eaa5aa089adeeb9bce013cbde611f
Your Name <you@example.com>
2026-07-19 18:25:46 +0200

    feat: add news listing and article pages

--- src/app/pages/article/article.ts
+++ src/app/pages/article/article.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-article',
-  imports: [],
+  imports: [RouterModule, TranslatePipe, CommonModule],
   templateUrl: './article.html',
-  styleUrl: './article.scss',
+  styleUrl: './article.scss'
 })
-export class Article {}
+export class Article {
+  slug = '';
+
+  constructor(private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(params => {
+      this.slug = params.get('slug') || '';
+    });
+  }
+}
